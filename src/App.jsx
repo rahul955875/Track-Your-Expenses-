36722ee43594042085b3eab7afacf2aab3bd0c49
rahul <nizarerahul7@gmail.com>
2025-02-04 18:37:06 +0530

    show data in table use FormData() constructor

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import './App.css'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseTable from './components/ExpenseTable'
+import expenseData from './expenseData'
+
+function App() {
+  const [expenses, setExpenses] = useState(expenseData)
+
+  return (
+    <>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm setExpenses = {setExpenses} />
+        <ExpenseTable expenses = {expenses}/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const ExpenseForm = ({setExpenses}) => {
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        setExpenses((preveState)=> [...preveState, {...getFormData(e.target), id : crypto.randomUUID()}])
+        e.target.reset()
+    }
+    const getFormData = (form)=>{
+        const formData = new FormData(form)
+        const data = {}
+        for (const [key,value] of formData.entries()) {
+            data[key] = value
+        }
+        return data
+    }
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+      <div className="input-container">
+        <label htmlFor="title">Title</label>
+        <input id="title" name="title" />
+      </div>
+      <div className="input-container">
+        <label htmlFor="category">Category</label>
+        <select id="category" name="category">
+          <option value="" hidden>
+            Select Category
+          </option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
+      </div>
+      <div className="input-container">
+        <label htmlFor="amount">Amount</label>
+        <input id="amount" name="amount" />
+      </div>
+      <button className="add-btn">Add</button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
