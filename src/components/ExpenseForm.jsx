5f9874df429641a7798f794f5bd83fe08b62b848
rahul <nizarerahul7@gmail.com>
2025-02-08 16:44:20 +0530

    implement form validation using useState

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,33 +1,69 @@
 import React, { useState } from "react";
 
-const ExpenseForm = ({setExpenses}) => {
+const ExpenseForm = ({ setExpenses }) => {
+  const [errors, setErrors] = useState({});
+
+  const validate = (formdata) => {
+    const errorsData = {};
+    if (Object.keys(errorsData).length) return;
+    if (!formdata.title) {
+      errorsData.title = "Please Enter Title";
+    }
+    if (!formdata.category) {
+      errorsData.category = "Please Enter Category";
+    }
+    if (!formdata.amount) {
+      errorsData.amount = "Please Enter amount";
+    }
+    setErrors(errorsData);
+    return errorsData;
+  };
   const [input, setinput] = useState({
-    title : '',
-    category : '',
-    amount : ''
-  })
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        setExpenses((preveState)=> [...preveState, {...input, id : crypto.randomUUID()}])
-       setinput({
-        title : '',
-        category : '',
-        amount : ''
-       })
-    }
-    const handleChange = (e)=>{
-      setinput({...input, [e.target.name]: e.target.value})
-    }
-    
+    title: "",
+    category: "",
+    amount: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validate(input)
+    const validateData = validate(input);
+    // console.log(validateData)
+    if (Object.keys(validateData).length) return;
+    setExpenses((preveState) => [
+      ...preveState,
+      { ...input, id: crypto.randomUUID() },
+    ]);
+    setinput({
+      title: "",
+      category: "",
+      amount: "",
+    });
+  };
+  const handleChange = (e) => {
+    setinput({ ...input, [e.target.name]: e.target.value });
+    setErrors({});
+  };
+
   return (
     <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlFor="title">Title</label>
-        <input type="text" value={input.title} name="title" onChange={handleChange} />
+        <input
+          type="text"
+          value={input.title}
+          name="title"
+          onChange={handleChange}
+        />
+        <p className="err">{errors.title}</p>
       </div>
       <div className="input-container">
         <label htmlFor="category">Category</label>
-        <select id="category" value={input.category} name="category" onChange={handleChange}>
+        <select
+          id="category"
+          value={input.category}
+          name="category"
+          onChange={handleChange}
+        >
           <option value="" hidden>
             Select Category
           </option>
@@ -37,10 +73,18 @@ const ExpenseForm = ({setExpenses}) => {
           <option value="education">Education</option>
           <option value="medicine">Medicine</option>
         </select>
+        <p className="err">{errors.category}</p>
       </div>
       <div className="input-container">
         <label htmlFor="amount">Amount</label>
-        <input type="number" id="amount" value={input.amount} name="amount" onChange={handleChange} />
+        <input
+          type="number"
+          id="amount"
+          value={input.amount}
+          name="amount"
+          onChange={handleChange}
+        />
+        <p className="err">{errors.amount}</p>
       </div>
       <button className="add-btn">Add</button>
     </form>
