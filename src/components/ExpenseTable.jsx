7514bfa63b2804a8b1e569468cd82e501092d267
rahul <nizarerahul7@gmail.com>
2025-02-17 22:36:39 +0530

    implement Ascending and Descending oreder funtionality in Expense table

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 
-const ExpenseTable = ({ expenses }) => {
+const ExpenseTable = ({ expenses, setExpenses }) => {
   const [query, setQuery] = useState("");
-
+  const handleAscending = () => {
+    // const AscendingExpneses = expenses.sort((a,b)=> a.amount - b.amount)
+    // setExpenses(AscendingExpneses)
+    const AscendingOrderExp = (expenses.toSorted((a, b) => a.amount - b.amount))
+    setExpenses(AscendingOrderExp)
+  };
+  const handleDescending = ()=>{
+    const DecendingOrderExp = expenses.toSorted((a,b)=> b.amount - a.amount)
+    setExpenses(DecendingOrderExp)
+  }
+  console.log("rendering");
   const totalAmount = expenses
     .filter((item) => item.category.toLowerCase().includes(query))
     .map((item) => Number(item.amount))
@@ -31,11 +41,13 @@ const ExpenseTable = ({ expenses }) => {
                 width="10"
                 viewBox="0 0 384 512"
                 className="arrow up-arrow"
+                onClick={handleAscending}
               >
                 <title>Ascending</title>
                 <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
               </svg>
               <svg
+              onClick={handleDescending}
                 xmlns="http://www.w3.org/2000/svg"
                 width="10"
                 viewBox="0 0 384 512"
