7c3d59cb1136d9d4eaf67720bd2b408a6d990308
rahul <nizarerahul7@gmail.com>
2025-03-01 21:14:06 +0530

    apply styling to table

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import ContextMenu from "./ContextMenu";
 
-const ExpenseTable = ({ expenses, setExpenses,formState,rows }) => {
-console.log(expenses)  
+const ExpenseTable = ({ expenses, setExpenses, formState, rows }) => {
+  console.log(expenses);
   const [query, setQuery] = useState("");
-  const [rowId, setRowId] = rows
+  const [rowId, setRowId] = rows;
   const [elementStyle, setElementStyle] = useState({});
-  const [sortCallback , setSortCallback]= useState(()=>()=>{})
+  const [sortCallback, setSortCallback] = useState(() => () => {});
   console.log("rendering");
   const totalAmount = expenses
     .filter((item) => item.category.toLowerCase().includes(query))
@@ -15,13 +15,26 @@ console.log(expenses)
 
   return (
     <>
-      <ContextMenu elementStyle={elementStyle} setExpenses={setExpenses} expenses={expenses} rowId={rowId} formState = {formState}  />
-      <table className="expense-table " onClick={()=>setElementStyle({})}>
+      <ContextMenu
+        elementStyle={elementStyle}
+        setExpenses={setExpenses}
+        expenses={expenses}
+        rowId={rowId}
+        formState={formState}
+      />
+      <table
+        className="expense-table table-hover table "
+        onClick={() => {
+          if (rowId) {
+            setElementStyle({});
+          }
+        }}
+      >
         <thead>
-          <tr>
-            <th>Title</th>
+          <tr className="table-success">
+            <th className="">Title</th>
             <th>
-              <select onChange={(e) => setQuery(e.target.value)}>
+              <select onChange={(e) => setQuery(e.target.value)} className="">
                 <option value="">All</option>
                 <option value="grocery">Grocery</option>
                 <option value="clothes">Clothes</option>
@@ -34,18 +47,21 @@ console.log(expenses)
               <div>
                 <span>Amount</span>
                 <svg
-                
                   xmlns="http://www.w3.org/2000/svg"
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
-                  onClick={()=>setSortCallback(()=>(a,b)=> a.amount-b.amount)}
+                  onClick={() =>
+                    setSortCallback(() => (a, b) => a.amount - b.amount)
+                  }
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
                 </svg>
                 <svg
-                  onClick={()=>setSortCallback(()=>(a,b)=> b.amount-a.amount)}
+                  onClick={() =>
+                    setSortCallback(() => (a, b) => b.amount - a.amount)
+                  }
                   xmlns="http://www.w3.org/2000/svg"
                   width="10"
                   viewBox="0 0 384 512"
@@ -60,14 +76,16 @@ console.log(expenses)
         </thead>
         <tbody>
           {expenses
-            .filter((item) => item.category.toLowerCase().includes(query)).sort(sortCallback)
+            .filter((item) => item.category.toLowerCase().includes(query))
+            .sort(sortCallback)
             .map(({ id, title, category, amount }) => (
               <tr
+              className="danger"
                 key={id}
                 onContextMenu={(e) => {
                   e.preventDefault();
                   setElementStyle({ left: e.clientX + 5, top: e.clientY + 5 });
-                  setRowId(id)
+                  setRowId(id);
                 }}
               >
                 <td>{title}</td>
@@ -77,7 +95,7 @@ console.log(expenses)
             ))}
           <tr>
             <th>Total</th>
-            <th onClick={()=>setSortCallback(()=>()=>{})}>Cancel Sort</th>
+            <th onClick={() => setSortCallback(() => () => {})}>Cancel Sort</th>
             <th>₹{totalAmount}</th>
           </tr>
         </tbody>
